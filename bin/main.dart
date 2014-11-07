fac0501f11da4757187345f016f317ab7cba2c2d
asgerf <asgerklasker@gmail.com>
2014-11-07 14:08:01 +0100

    ..

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,4 @@
-import 'jscallgraphs.dart';
+import 'flowgraph.dart';
 import 'package:parsejs/parsejs.dart';
 import 'dart:io';
 
@@ -8,10 +8,25 @@ void main(List<String> args) {
     Program ast = parsejs(code, filename: filename);
     FlowGraph flowGraph = buildFlowGraph(ast);
     
+    new File('flowgraph.dot').writeAsString(flowGraph.toDot());
+    
+    String functionName(FunctionNode node) {
+      if (node.name != null) {
+        return node.name.value;
+      }
+      if (node.parent is FunctionExpression && node.parent.parent is AssignmentExpression) {
+        AssignmentExpression assign = node.parent.parent;
+        if (assign.left.line == node.line) {
+          return code.substring(assign.left.start, assign.left.end).replaceAll('.prototype.', '::');
+        }
+      }
+      return '<anon>:${node.line}';
+    }
+    
     findCalls(Node node) {
       if (node is CallExpression) {
         List<FunctionNode> targets = flowGraph.findCallTargets(node);
-        print("${node.location}: ${targets}");
+        print("${node.location}: ${targets.map(functionName).join(', ')}");
       }
       node.forEach(findCalls);
     }
